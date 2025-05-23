0c374b83bf58d2a63ffa9f2b8009bb122a4dc5b0
عبدالمنان <msmannan00@protonmail.com>
2025-05-23 16:01:14 +0500

    Update server.py

--- app/api/server.py
+++ app/api/server.py
@@ -10,35 +10,22 @@ from .ocr_manager.ocr_emums import OCR_REQUEST_COMMANDS
 from .runtime_parse_manager.runtime_parse_controller import runtime_parse_controller
 from .topic_manager.topic_classifier_controller import topic_classifier_controller
 from .topic_manager.topic_classifier_enums import TOPIC_CLASSFIER_COMMANDS, TOPIC_CATEGORIES
+from fastapi import FastAPI, Request, HTTPException, UploadFile, File
+
+import concurrent.futures
 import asyncio
 import logging
-import threading
-from fastapi import FastAPI, Request, HTTPException, UploadFile, File
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
 
-async def run_with_cancellation(request: Request, func, timeout: int, default_result):
-    stop_event = threading.Event()
-    task = asyncio.create_task(asyncio.to_thread(func, stop_event))
-    try:
-        while not task.done():
-            if await request.is_disconnected():
-                stop_event.set()
-                task.cancel()
-                return default_result
-            await asyncio.sleep(0.1)
-        return await asyncio.wait_for(task, timeout=timeout)
-    except asyncio.TimeoutError:
-        stop_event.set()
-        return default_result
-    except Exception:
-        stop_event.set()
-        raise
-
 class APIService:
 
     def __init__(self):
+        executor = concurrent.futures.ThreadPoolExecutor(max_workers=50)
+        loop = asyncio.get_event_loop()
+        loop.set_default_executor(executor)
+
         self.app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
         self.m_runtime_parser = runtime_parse_controller()
         self.m_cti_parser = cti_classifier_controller()
@@ -59,13 +46,18 @@ class APIService:
         self.app.add_api_route("/runtime/parse", self.runtime_parse, methods=["POST"])
         self.app.add_api_route("/ocr/parse", self.ocr_parse, methods=["POST"])
 
-    async def cti_classify(self, request: parse_cti_model, raw_request: Request):
+    async def cti_classify(self, request: parse_cti_model):
         logger.info("Received request at /cti_classifier/classify")
         async with self.semaphore:
-            func = lambda stop_event: self.m_cti_parser.invoke_trigger(CTI_REQUEST_COMMANDS.S_PARSE, [request.data], stop_event=stop_event)
             try:
-                result = await run_with_cancellation(raw_request, func, timeout=15, default_result={"result": {"label": "none"}})
-                return result
+                result = await asyncio.wait_for(
+                    asyncio.to_thread(lambda: self.m_cti_parser.invoke_trigger(CTI_REQUEST_COMMANDS.S_PARSE, [request.data])),
+                    timeout=15
+                )
+                return {"result": result}
+            except asyncio.TimeoutError:
+                logger.warning("CTI classification request timed out.")
+                return {"result": {"label": "none"}}
             except Exception:
                 logger.error("Error occurred while processing CTI classification request.", exc_info=True)
                 return {"result": {"label": "none"}}
@@ -74,77 +66,89 @@ class APIService:
         try:
             payload = await request.json()
             query: Dict[str, str] = payload.get("text", {})
+
             if not query or all(value == "" for value in query.values()):
                 raise HTTPException(status_code=400, detail="No valid query parameters provided or all values are empty")
-            response = await asyncio.wait_for(self.m_runtime_parser.get_email_username(query), timeout=120)
+
+            response = await asyncio.wait_for(
+                self.m_runtime_parser.get_email_username(query),
+                timeout=120
+            )
             return response
+
         except asyncio.TimeoutError:
             raise HTTPException(status_code=504, detail="Request timed out while processing")
         except Exception:
             logger.error("Exception occurred during runtime parse", exc_info=True)
             raise HTTPException(status_code=500, detail="An error occurred while processing the request")
 
-    async def process_request(self, request_data, raw_request: Request, command, controller, default_result, timeout=60):
+    async def process_request(self, request, command, controller, default_result, timeout=60):
         async with self.semaphore:
-            func = lambda stop_event: controller(command, request_data, stop_event=stop_event)
             try:
-                result = await run_with_cancellation(raw_request, func, timeout=timeout, default_result={"result": default_result})
-                return result
+                result = await asyncio.wait_for(
+                    asyncio.to_thread(lambda: controller(command, request)),
+                    timeout=timeout
+                )
+                return {"result": result}
+            except asyncio.TimeoutError:
+                logger.warning(f"Request for command '{command}' timed out after {timeout} seconds. Returning default result.")
+                return {"result": default_result}
             except Exception:
                 logger.error(f"An error occurred while processing request for command '{command}'. Returning default result.", exc_info=True)
                 return {"result": default_result}
 
-    async def nlp_parse(self, request: parse_request_model, raw_request: Request):
+    async def nlp_parse(self, request: parse_request_model):
         logger.info("Received request at /nlp/parse")
         return await self.process_request(
-            request_data=[request.data],
-            raw_request=raw_request,
+            request=[request.data],
             command=NLP_REQUEST_COMMANDS.S_PARSE,
             controller=self.nlp_controller_instance.invoke_trigger,
             default_result={}
         )
 
-    async def nlp_parse_ai(self, request: parse_request_model, raw_request: Request):
+    async def nlp_parse_ai(self, request: parse_request_model):
         logger.info("Received request at /nlp/parse/ai")
         return await self.process_request(
-            request_data=[request.data],
-            raw_request=raw_request,
+            request=[request.data],
             command=NLP_REQUEST_COMMANDS.S_PARSE_AI,
             controller=self.nlp_controller_instance.invoke_trigger,
             default_result={}
         )
 
-    async def nlp_summarise_ai(self, request: parse_request_model, raw_request: Request):
+    async def nlp_summarise_ai(self, request: parse_request_model):
         logger.info("Received request at /nlp/summarize/ai")
         return await self.process_request(
-            request_data=[request.data],
-            raw_request=raw_request,
+            timeout=60,
+            request=[request.data],
             command=NLP_REQUEST_COMMANDS.S_SUMMARIZE_AI,
             controller=self.nlp_controller_instance.invoke_trigger,
             default_result={}
         )
 
-    async def topic_classifier_predict(self, request: classify_request_model, raw_request: Request):
+    async def topic_classifier_predict(self, request: classify_request_model):
         logger.info("Received request at /topic_classifier/predict")
         return await self.process_request(
-            request_data=[request.title, request.description, request.keyword],
-            raw_request=raw_request,
+            request=[request.title, request.description, request.keyword],
             command=TOPIC_CLASSFIER_COMMANDS.S_PREDICT_CLASSIFIER,
             controller=self.topic_classifier_instance.invoke_trigger,
-            default_result=TOPIC_CATEGORIES.S_THREAD_CATEGORY_GENERAL
+            default_result=[TOPIC_CATEGORIES.S_THREAD_CATEGORY_GENERAL]
         )
 
-    async def ocr_parse(self, file: UploadFile = File(...), request: Request = None):
+    async def ocr_parse(self, file: UploadFile = File(...)):
         logger.info("Received request at /ocr/parse")
         content = await file.read()
         if len(content) > 50 * 1024 * 1024:
             raise HTTPException(status_code=413, detail="File too large. Maximum allowed size is 50MB")
+
         async with self.semaphore:
-            func = lambda stop_event: self.ocr_controller_instance.invoke_trigger(OCR_REQUEST_COMMANDS.S_PARSE, [content], stop_event=stop_event)
             try:
-                result = await run_with_cancellation(request, func, timeout=60, default_result={"text": ""})
+                result = await asyncio.wait_for(
+                    asyncio.to_thread(lambda: self.ocr_controller_instance.invoke_trigger(OCR_REQUEST_COMMANDS.S_PARSE, [content])),
+                    timeout=60
+                )
                 return {"text": result}
             except asyncio.TimeoutError:
+                logger.warning("OCR parsing timed out.")
                 raise HTTPException(status_code=504, detail="OCR request timeout")
             except Exception:
                 logger.error("Exception occurred during OCR parsing", exc_info=True)
