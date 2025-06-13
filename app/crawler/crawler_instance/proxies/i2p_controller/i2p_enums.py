cc292887b2bebec78dfb3378c726aca8ac4f3b1f
عبدالمنان <msmannan00@protonmail.com>
2025-06-14 01:04:49 +0500

    Update i2p_enums.py

--- app/crawler/crawler_instance/proxies/i2p_controller/i2p_enums.py
+++ app/crawler/crawler_instance/proxies/i2p_controller/i2p_enums.py
@@ -6,4 +6,8 @@ class I2P_COMMANDS:
 class I2P_PROXY:
   PROXY_URL_HTTP = "http://trusted-micros-i2p:8444"
   PROXY_URL_HTTPS = "http://trusted-micros-i2p:8445"
-  SUBSCRIPTION_URLS = ["http://i2p-projekt.i2p/hosts.txt", "http://inr.i2p/hosts.txt", "http://notbob.i2p/hosts.txt", "http://skank.i2p/hosts.txt", "http://stats.i2p/hosts.txt"]
+  SUBSCRIPTION_URLS = ["http://i2p-projekt.i2p/hosts.txt",
+                       "http://inr.i2p/hosts.txt",
+                       "http://notbob.i2p/hosts.txt",
+                       "http://skank.i2p/hosts.txt",
+                       "http://stats.i2p/hosts.txt"]
